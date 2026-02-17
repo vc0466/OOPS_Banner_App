37b98039e55ec56af3ff1c364ab9a6303518aba5
vc0466 <vc0466@srmist.edu.in>
2026-02-17 13:43:52 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,17 @@
 public class OOPSBannerApp{
 	public static void main(String[] args){
-		System.out.println(String.join(" ","   ***   ", "   ***   ", "******    ", "   ***** "));
-		System.out.println(String.join(" "," **   ** ", " **   ** ", "**    **  ", " **      "));
-		System.out.println(String.join(" "," **   ** ", " **   ** ", "**     ** ", "**       "));
-		System.out.println(String.join(" "," **   ** ", " **   ** ", "**    **  ", " **      "));
-		System.out.println(String.join(" "," **   ** ", " **   ** ", "******    ", "   ***   "));
-		System.out.println(String.join(" "," **   ** ", " **   ** ", "**        ", "      ** "));
-		System.out.println(String.join(" "," **   ** ", " **   ** ", "**        ", "       **"));
-		System.out.println(String.join(" ","  ** **  ", "  ** **  ", "**        ", "      ** "));
-		System.out.println(String.join(" ","   ***   ", "   ***   ", "**        ", " *****   "));
+		String[] lines = new String[9];
+		lines[0] = String.join(" ","   ***   ", "   ***   ", "******    ", "   ***** ");
+		lines[1] = String.join(" "," **   ** ", " **   ** ", "**    **  ", " **      ");
+		lines[2] = String.join(" "," **   ** ", " **   ** ", "**     ** ", "**       ");
+		lines[3] = String.join(" "," **   ** ", " **   ** ", "**    **  ", " **      ");
+		lines[4] = String.join(" "," **   ** ", " **   ** ", "******    ", "   ***   ");
+		lines[5] = String.join(" "," **   ** ", " **   ** ", "**        ", "      ** ");
+		lines[6] = String.join(" "," **   ** ", " **   ** ", "**        ", "       **");
+		lines[7] = String.join(" ","  ** **  ", "  ** **  ", "**        ", "      ** ");
+		lines[8] = String.join(" ","   ***   ", "   ***   ", "**        ", " *****   ");
+		for (String line : lines) {
+			System.out.println(line);
+		}
 	}
 }
